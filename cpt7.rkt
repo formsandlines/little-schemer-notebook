361c4a8cb32a6353a540f565b96b04fac4da6800
Peter Hofmann <peter.hofmann@formsandlines.eu>
2023-03-29 00:12:05 +0200

    Progress in Chapter 7

--- cpt7.rkt
+++ cpt7.rkt
@@ -57,3 +57,81 @@
 
 (equal? (makeset '(apple 3 pear 4 9 apple 3 4))
         '(apple 3 pear 4 9))
+
+(define (-subset-alt1 set1 set2)
+  (cond ((null? set1) #t)
+        ((member? (car set1) set2) (-subset-alt1 (cdr set1) set2))
+        (else #f)))
+
+(define (-subset? set1 set2)
+  (cond ((null? set1) #t)
+        (else (and (member? (car set1) set2)
+                   (-subset? (cdr set1) set2)))))
+
+(eq? (-subset? '(5 chicken wings)
+               '(5 hamburgers
+                 2 pieces of fried chicken and light duckling wings))
+     #t)
+
+(eq? (-subset? '(4 pounds of horseradish)
+               '(four pounds chicken and
+                 5 ounces horseradish))
+     #f)
+
+(define (eqset? set1 set2)
+  (and (-subset? set1 set2)
+       (-subset? set2 set1)))
+
+(eq? (eqset? '(6 large chickens with wings)
+             '(6 chickens with large wings))
+     #t)
+
+(define (intersect?-alt1 set1 set2)
+  (cond ((or (null? set1)
+             (null? set2)) #f)
+        ((member? (car set1) set2) #t)
+        (else (intersect?-alt1 (cdr set1) set2))))
+
+(define (intersect? set1 set2)
+  (cond ((or (null? set1)
+             (null? set2)) #f)
+        (else (or (member? (car set1) set2)
+                  (intersect? (cdr set1) set2)))))
+
+(eq? (intersect? '(stewed tomatoes and macaroni)
+                 '(macaroni and cheese))
+     #t)
+
+(define (intersect set1 set2)
+  (cond ((null? set1) '())
+        ((member? (car set1) set2) (cons (car set1)
+                                         (intersect (cdr set1) set2)))
+        (else (intersect (cdr set1) set2))))
+
+(equal? (intersect '(stewed tomatoes and macaroni)
+                   '(macaroni and cheese))
+        '(and macaroni))
+
+(define (union-alt1 set1 set2)
+  (makeset (append set1 set2)))
+
+(define (union-alt2 set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else (cons (car set1)
+                    (union-alt2 (cdr set1)
+                                (rember (car set1) set2))))))
+
+(equal? (union-alt2 '(stewed tomatoes and macaroni casserole)
+                    '(macaroni and cheese))
+        '(stewed tomatoes and macaroni casserole cheese))
+
+(define (union set1 set2)
+  (cond ((null? set1) set2)
+        ((member? (car set1) set2) (union (cdr set1) set2))
+        (else (cons (car set1)
+                    (union (cdr set1) set2)))))
+
+(equal? (union '(stewed tomatoes and macaroni casserole)
+               '(macaroni and cheese))
+        '(stewed tomatoes casserole macaroni and cheese))
